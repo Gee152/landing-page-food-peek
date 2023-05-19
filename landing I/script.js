3e7efe953dd6171ee56ecdab2ead719d4def3fa0
gabriel <gabrielvictos152@gmail.com>
2023-05-19 18:45:34 -0300

    subindo projeto

---
+++ landing I/script.js
@@ -0,0 +1,10 @@
+
+
+
+const menu = document.querySelector('.menu');
+const navMenu = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('ativo');
+    navMenu.classList.toggle('ativo');
+});
